68be7928d9b1d0c2255f69d4a0545061973c26e5
Joachim Metz <joachim.metz@gmail.com>
2022-02-04 05:59:32 +0100

    Applied updates

--- pygzipf/pygzipf_members.h
+++ pygzipf/pygzipf_members.h
@@ -1,7 +1,7 @@
 /*
  * Python object definition of the sequence and iterator object of members
  *
- * Copyright (C) 2019-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2019-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
