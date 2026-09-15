c7021a115443bc544d6b08cd1dd1e0b06a34fc3e
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 09:28:33 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libgzipf/.libs/libgzipf.1.dylib && test -f ./pygzipf/.libs/pygzipf.so
+if [ -f "${PWD}/libgzipf/.libs/libgzipf.1.dylib" ] && [ -f ./pygzipf/.libs/pygzipf.so ]
 then
-	install_name_tool -change /usr/local/lib/libgzipf.1.dylib ${PWD}/libgzipf/.libs/libgzipf.1.dylib ./pygzipf/.libs/pygzipf.so
+    install_name_tool -change /usr/local/lib/libgzipf.1.dylib "${PWD}/libgzipf/.libs/libgzipf.1.dylib" ./pygzipf/.libs/pygzipf.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
